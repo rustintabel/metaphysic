f8c85e41caa4033627eace45655957ab16edb1b8
justin.gabel@gmail.com <justin.gabel@gmail.com@b46a0291-0291-8aed-3814-75fff0452644>
2013-06-06 21:50:05 +0000

    Personal heuristics as comments.

---
+++ trunk/metaphysic/ndart/src/metaphysic/LifestyleProjectionOfStrongAnthropicGaliosGroup.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaphysic;
+
+/**
+ *
+ * @author justin
+ * 
+ * Visually each GaliosGroup can be projected such that the Nucleous asthetic circle
+ * is inside the Brain Asthetic Circle and back to back with these are the two corresponding
+ * Printmaking Asthetics of The lifstyle inside the World asthetic circle 
+ * (personal space of community). 
+ * 
+ * This makes an ideal day planner and visualizer of whatever goes into it.
+ */
+public class LifestyleProjectionOfStrongAnthropicGaliosGroup {
+    
+}
