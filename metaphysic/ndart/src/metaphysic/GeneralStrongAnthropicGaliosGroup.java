25e2e453cb69b613ad5965c556194f718b8c87e5
justin.gabel <justin.gabel@gmail.com>
2013-06-06 20:17:04 +0000

    Personal heuristics as comments.

---
+++ metaphysic/ndart/src/metaphysic/GeneralStrongAnthropicGaliosGroup.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaphysic;
+
+/**
+ *
+ * @author justin
+ */
+public class GeneralStrongAnthropicGaliosGroup {
+    /**Each layer of complexity from organisms to atoms are composed as a fractle.
+    * one layer fights entropy as a sort of machien and the layer above adds
+    * a governing force on the one below as it is within the tissues of the organs controled
+    * by a brain in which the nich comfortable for the life of the cell that the cell lives.
+    * From below the layer is also managed by the modularity of the organs of its cells.
+    * And so each layer adds fecundity.
+}
