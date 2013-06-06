299df17b82c12ebdb032edfbd47a816902eecc2f
justin.gabel <justin.gabel@gmail.com>
2013-06-06 18:41:59 +0000

    Personal heuristics as comments.

--- metaphysic/ndart/src/metaphysic/Asthetic.java
+++ metaphysic/ndart/src/metaphysic/Asthetic.java
@@ -65,10 +65,37 @@ public abstract class Asthetic {
     //through the reproduction layer which is a map from brain to world
     //consisting of organs mirroring the electrons of the atoms.
 
+    //Abstractly each of the four quadrants together complete one iteration of an 
+    //asthetic circle. Each moves information in two dimentions to the next quadrant and 
+    //enacts variations on a theme from its own modular structur through a physically connected
+    //map of switches to nerves.
+    //The two dimentions here are best represented by x is brain,  body, sense layer
+    //And y tissues of cells composed into modular organs into cell like bodies with brains.
+    //A group of which is again a tissue. The large brain made of the small ones becase
+    //smaller is faster.
+    
  //for each of the following do one iteration of thoes which have some work to do   
     public abstract void hand();
+    //Like the eye humunculous the hand trains its cells and muscle is the equivalent of memory.
+    //The variations on a theme of this quadrant build constructions from existing structures
+    //Parts with parts. Where the eye humunculous builds abstractly the hand builds 
+    //physically.
+    
     public abstract void handHumunculous();
+    //This is creative rather than critical
+    //It is only made efficent enough by modularity of the brain mapped to organs
+    //mapped to your senses and closed system personal space.
+    //Variations on a theme agagin.
+    //This quadrant initiates a cycle of the asthetic and the basic ideas
+    //of rapid prototyping, iteration and diminishing returns must be considered.
+    //This is just the logic of a cycle of improving intelligence in a body 
+    //from a controller to a physical outside edge.
+    
     public abstract void eye();
+    //At this quadrant your personal space or a peer is initiating a creative act 
+    //recived by a sense organ. Moduals of the organ of the sense layer (physical outside surface)
+    //Mapped to a physical connection 
+   
     public abstract void eyeHumunculous();
     //Critique:
     //Rapid prototyping iteration and then diminishing returns can also
@@ -78,7 +105,8 @@ public abstract class Asthetic {
     //actions the consequences of which lead to a new symetry shell are godlike
     //as Stephen Hawking says The universe must be the way it is for us to be here 
     //asking why
-    //as food for thought consider the way fine distinctions are both streangthened by
+    //as food for thought consider the way fine distinctions (like beuty and truth) 
+    //are both streangthened by
     //looking criticaly at variations on a theme and empowering. 
     //this seems naturally fair
     //this quadrant terminates every iteration it is the clean up and making judgements. 
