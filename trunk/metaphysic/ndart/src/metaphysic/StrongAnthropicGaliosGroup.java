83c64e74c22e95d71c77be1095c5af21ff869df0
justin.gabel@gmail.com <justin.gabel@gmail.com@b46a0291-0291-8aed-3814-75fff0452644>
2013-06-09 07:34:59 +0000

    Galios group

--- trunk/metaphysic/ndart/src/metaphysic/StrongAnthropicGaliosGroup.java
+++ trunk/metaphysic/ndart/src/metaphysic/StrongAnthropicGaliosGroup.java
@@ -48,9 +48,11 @@ public class StrongAnthropicGaliosGroup {
             RecombinationAsthetic CellPeerRecombinationAsthetic,
             ReproductionAsthetic CellPeerReproductionAsthetic,
             PrintMakingAsthetic CellPeerPrintMakingAsthetic,
+            
             RecombinationAsthetic CellRecombinationAsthetic,
             ReproductionAsthetic CellReproductionAsthetic,
             PrintMakingAsthetic CellPrintMakingAsthetic,
+            
             RecombinationAsthetic CellTissueRecombinationAsthetic,
             ReproductionAsthetic CellTissueReproductionAsthetic,
             PrintMakingAsthetic CellTissuePrintMakingAsthetic
@@ -59,13 +61,60 @@ public class StrongAnthropicGaliosGroup {
             this.CellPeerRecombinationAsthetic=CellPeerRecombinationAsthetic;
             this.CellPeerReproductionAsthetic=CellPeerReproductionAsthetic;
             this.CellPeerPrintMakingAsthetic=CellPeerPrintMakingAsthetic;
+            
             this.CellRecombinationAsthetic=CellRecombinationAsthetic;
             this.CellReproductionAsthetic=CellReproductionAsthetic;
             this.CellPrintMakingAsthetic=CellPrintMakingAsthetic;
+            
             this.CellTissueRecombinationAsthetic=CellTissueRecombinationAsthetic;
             this.CellTissueReproductionAsthetic=CellTissueReproductionAsthetic;
             this.CellTissuePrintMakingAsthetic=CellTissuePrintMakingAsthetic;
+            
+    //Of all the quadrants of all the three fundamental asthetics mirrored at each symetry shell.
+    //two phenomina are repeated. Analogsly they are hands and eyes. occupying the same eight
+    //quadrants. And a map can be made of this. This is the composistion of mebrane information 
+            //feilds into organs controllable by the brain. 
+            
+            //Each quadrant is the interaction of two tissue information fields.
+            //Each such feild conducts a cascade of information chemical or otherwise
+            //as each cell of the tissue responds in like to a signal.
+            //Where such a cascade meets another tissue information can be passed from
+            //one such cell type to the other forming a circut between the two.
+            //Such a circut consists of a transistor like component where the receptor of the second
+            //cell defines the asthetic and so depending on thhe type of cell in spite of the 
+            //organ its self serving the function of the asthetic befor it it will need to
+            //use information from the same moduals of the destination cell to transmit information
+            //to it. Since the cell is modular with its brain body and receptors this is efficennt.
+            //This order is then reversed going into versus out of the brain duue to the cell
+            //type order being reversed. and so for example if represennting recombination with 
+            //a heart (sexual selection) and reproduction with a hand (muscle and mitosis) and finally
+            //symbolizing printmaking with a foot print. Then the brain Asthetic becomes
+            //heart hand foot heart    and it community containing it then becomes
+            //foot heart hand heart
+            //
+            //the pattern being 
+            //+   &   $   -*
+            //*   +   &   -$ 
+            //This pattern is repeated over and over to make the information field of life
+            //which is a galios group as these automorphisms are composed into tissues and organs
+            //which feilds in which informmation flows freely.
+            
+                        //Brain
+            //eye for an eye
+            this.CellRecombinationAsthetic.eye=this.CellTissueRecombinationAsthetic.eye;
+            
+                                /*
+                    =CellRecombinationAsthetic;this.CellRecombinationAsthetic
+            this.CellReproductionAsthetic=CellReproductionAsthetic;
+            this.CellPrintMakingAsthetic=CellPrintMakingAsthetic;
+            
+            this.CellTissueRecombinationAsthetic=CellTissueRecombinationAsthetic;
+            this.CellTissueReproductionAsthetic=CellTissueReproductionAsthetic;
+            this.CellTissuePrintMakingAsthetic=CellTissuePrintMakingAsthetic;
+    */
     }
     
-    
+
+
+
 }
