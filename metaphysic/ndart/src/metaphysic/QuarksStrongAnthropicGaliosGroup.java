4c592abbabea52aacd2ce09aa36f54277922a457
justin.gabel <justin.gabel@gmail.com>
2013-06-14 03:47:34 +0000

    Galios group

---
+++ metaphysic/ndart/src/metaphysic/QuarksStrongAnthropicGaliosGroup.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package metaphysic;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author justin
+ */
+public class QuarksStrongAnthropicGaliosGroup extends StrongAnthropicGaliosGroup{
+    
+            
+            
+                    
+    public ArrayList<SurfaceCovalentShellProbabilityDistributionFunctionAsthetic> 
+            SurfaceCovalentShellProbabilityDistributionFunctionAstheticTypes
+            =new ArrayList<SurfaceCovalentShellProbabilityDistributionFunctionAsthetic>();
+    public ArrayList<ElectronProbabilityDistributionFunctionAsthetic> 
+            ElectronProbabilityDistributionFunctionAstheticTypes
+            =new ArrayList<ElectronProbabilityDistributionFunctionAsthetic>();
+    public ArrayList<NuclearProbabilityDistributionFunctionAsthetic> 
+            NuclearProbabilityDistributionFunctionAstheticTypes
+            =new ArrayList<NuclearProbabilityDistributionFunctionAsthetic>();
+    
+    public ArrayList<PrintMakingAsthetic> PrintMakingAstheticTypes
+            =new ArrayList<PrintMakingAsthetic>();
+    public ArrayList<UpQuarkAshetic> UpQuarkAsheticTypes
+            =new ArrayList<UpQuarkAshetic>(); 
+    public ArrayList<DownQuarkAsthetic> DownQuarkAstheticTypes
+            =new ArrayList<DownQuarkAsthetic>();
+    
+    public ArrayList<ElectromagneticForceAsthetic> ElectromagneticForceAstheticTypes
+            =new ArrayList<ElectromagneticForceAsthetic>();
+    public ArrayList<PhotonAsthetic> PhotonAstheticTypes
+            =new ArrayList<PhotonAsthetic>();
+    public ArrayList<BaryonSymmetry> BaryonSymmetryTypes
+            =new ArrayList<BaryonSymmetry>();
+            
+}
